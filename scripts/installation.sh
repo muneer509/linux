0ac94ea6b33ca96907bae77fcc3c114c10d1f45b
Muneer Shaik <mshaik4@berkadia.com>
2025-08-10 16:55:16 +0530

    practice

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-#check the user has root access or not
+
 USERID=$(id -u)
-if [ $USERID != 0 ]
-then
-echo "You dont have Admin privilies to run this script, Please run with Admin access"
-exit 1
 
-fi
-apt list mysql
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
+    fi
+}
+
+CHECK_ROOT
+
+dnf list installed git
+
 if [ $? -ne 0 ]
 then
-echo "mysql not installed,going to install nginx"
-apt install mysql -y
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql
+
 if [ $? -ne 0 ]
-then 
-echo "mysql installation is failure"
-exit 1
+then
+    echo "MySQL is not installed...going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
 else
-echo "mysql installation is success"
+    echo "MySQL is already installed..nothing to do"
 fi
-else
-echo "mysql is already installed"
-
-fi
-
-
-
