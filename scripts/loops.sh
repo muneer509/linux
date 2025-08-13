98075de0a9df986a784a87b7bb432c4c8bbdcfdf
Muneer Shaik <mshaik4@berkadia.com>
2025-08-13 14:59:47 +0530

    practice

--- scripts/loops.sh
+++ scripts/loops.sh
@@ -9,4 +9,23 @@ if [ $ID -ne 0 ]
 then
 echo "Please install this script with root user access"
 fi
+VALIDATE()
+if [ $1 -ne 0 ]
+  then
+    echo "$2 id FAILD"
+  else
+    echo "$2 installation is success"
+fi  
+
+for $package in $@
+do 
+    apt list installed @package
+    if [ $? -ne 0 ]
+    then
+        echo "$package not installed.."
+        apt install @package -y
+        VALIDATE $? "installing $package"
+    else
+        echo "$package already installed"
+    fi
 
