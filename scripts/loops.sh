4e00e577b9687619e4190dbf93550f32fa2f66e0
Muneer Shaik <mshaik4@berkadia.com>
2025-08-13 14:11:50 +0530

    practice

--- scripts/loops.sh
+++ scripts/loops.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-for i in $(seq 1 100)
-do
-  echo $i
-done
+# for i in $(seq 1 100)
+# do
+#   echo $i
+# done
+ROOTID=(id -u)
+if [ $ROOTID -ne 0]
+then
+echo "Please install this script with root user access"
+fi
+
