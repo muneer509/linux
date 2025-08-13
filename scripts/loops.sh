50f64f66eb18297806362957bdb4fa01c9182bc9
Muneer Shaik <mshaik4@berkadia.com>
2025-08-13 15:03:47 +0530

    practice

--- scripts/loops.sh
+++ scripts/loops.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-# for i in $(seq 1 100)
-# do
-#   echo $i
-# done
-ID=$(id -u)
-if [ $ID -ne 0 ]
-then
-echo "Please install this script with root user access"
-fi
-VALIDATE()
-if [ $1 -ne 0 ]
-  then
-    echo "$2 id FAILD"
-  else
-    echo "$2 installation is success"
-fi  
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-for $package in $@
-do 
-    apt list installed @package
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT
+
+# sh 15-loops.sh git mysql postfix nginx
+for package in $@ # $@ refers to all arguments passed to it
+do
+    dnf list installed $package
     if [ $? -ne 0 ]
     then
-        echo "$package not installed.."
-        apt install @package -y
-        VALIDATE $? "installing $package"
+        echo "$package is not installed, going to install it.."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
     else
-        echo "$package already installed"
+        echo "$package is already installed..nothing to do"
     fi
-
+done
